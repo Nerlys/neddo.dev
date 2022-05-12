1b9f2783cd152251c6e0360df628429cc82305a9
Sven <me@neddo.dev>
2022-05-12 19:56:56 +0200

    footer fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ export function Footer() {
         </div>
         <div className="grid grid-cols-2 gap-6 mt-5">
           <div className="grid grid-flow-row gap-3">
-            <div className="opacity-50 hover:opacity-100 duration-200"><NextLink href={"/home"}>Home</NextLink></div>
+            <div className="opacity-50 hover:opacity-100 duration-200"><NextLink href={"./"}>Home</NextLink></div>
             <div className="opacity-50 hover:opacity-100 duration-200"><NextLink href={"/about"}>About</NextLink></div>
             <div className="opacity-50 hover:opacity-100 duration-200"><NextLink href={"/blog"}>Blog</NextLink></div>
           </div>
